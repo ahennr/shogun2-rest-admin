394042bf8044f9cf4f18d1505b4f251981490f08
Andre Henn <henn@terrestris.de>
2017-02-15 17:39:57 +0100

    wip

--- src/auth/authClient.js
+++ src/auth/authClient.js
@@ -1,19 +1,26 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK } from 'admin-on-rest';
+import { AUTH_LOGIN } from 'admin-on-rest';
+import {queryParameters} from '../shogun2restclient/fetch'
 
 export default (type, params) => {
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        debugger
-        return Promise.resolve();
+        const { username, password } = params;
+        const request = new Request('http://localhost:8080/momo/doLogin', {
+            method: 'POST',
+            body: queryParameters({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
+        })
+        return fetch(request)
+            .then(response => {
+              console.log(response)
+                if (response.status < 200 || response.status >= 400) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                debugger
+                localStorage.setItem('token', token)
+            });
     }
-    if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username');
-        return Promise.resolve();
-    }
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('username') ? Promise.resolve() : Promise.reject();
-    }
-    return Promise.reject('Unkown method');
-};
+    return Promise.resolve();
+}
