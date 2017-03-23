95b739adb646c5bb681e8769edcafe0ebc9c54db
Andre Henn <henn@terrestris.de>
2017-03-23 16:18:56 +0100

    change to webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports =  {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    app: [
+      './src/index.js'
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js',
+  },
+
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 3000
+  },
+
+  // resolve: {
+  //   alias: {
+  //     'ag-grid-root': __dirname + '/node_modules/ag-grid'
+  //   }
+  // },
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      // query: {
+      //   presets: [
+      //     'es2015',
+      //     'react'
+      //   ]
+      // }
+    }, {
+      test: /\.scss$/,
+      loaders: [
+        'style',
+        'css',
+        'sass'
+      ]
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './src/sass')]
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: './node_modules/openlayers/dist/ol.js'
+    }, {
+      from: './node_modules/openlayers/dist/ol.css'
+    }
+    ])
+  ]
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,21 @@
+var devConfig = require('./webpack.config.js');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var prodConfig = Object.assign({}, devConfig);
+
+prodConfig.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new CopyWebpackPlugin([{
+    from: 'app/index.html'
+  },{
+    from: 'app/resources',
+    to: 'resources'
+  }])
+);
+
+module.exports = prodConfig;
